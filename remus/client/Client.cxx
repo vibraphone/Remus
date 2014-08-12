10d28a76ca41b654ea79ad1d837927ae58677b3c
Robert Maynard <robert.maynard@kitware.com>
2014-08-12 14:59:23 -0400

    Always treat Response's data as being a binary data stream.
    If we blindly convert response.data() into a string, without using
    the length parameter we break sending binary data as the string will
    be terminated by the first null terminator.

--- remus/client/Client.cxx
+++ remus/client/Client.cxx
@@ -113,7 +113,7 @@ Client::submitJob(const remus::proto::JobSubmission& submission)
   j.send(&this->Zmq->Server);
 
   remus::proto::Response response(&this->Zmq->Server);
-  const std::string job = response.data();
+  const std::string job(response.data(), response.dataSize());
   return remus::proto::to_Job(job);
 }
 
@@ -126,7 +126,7 @@ remus::proto::JobStatus Client::jobStatus(const remus::proto::Job& job)
   j.send(&this->Zmq->Server);
 
   remus::proto::Response response(&this->Zmq->Server);
-  const std::string status = response.data();
+  const std::string status(response.data(), response.dataSize());
   return remus::proto::to_JobStatus(status);
 }
 
@@ -139,7 +139,7 @@ remus::proto::JobResult Client::retrieveResults(const remus::proto::Job& job)
   j.send(&this->Zmq->Server);
 
   remus::proto::Response response(&this->Zmq->Server);
-  const std::string result = response.data();
+  const std::string result(response.data(), response.dataSize());
   return remus::proto::to_JobResult(result);
 }
 
@@ -152,7 +152,7 @@ remus::proto::JobStatus Client::terminate(const remus::proto::Job& job)
   j.send(&this->Zmq->Server);
 
   remus::proto::Response response(&this->Zmq->Server);
-  const std::string status = response.data();
+  const std::string status(response.data(), response.dataSize());
   return remus::proto::to_JobStatus(status);
 }
 
